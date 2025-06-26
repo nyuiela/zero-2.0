289db2b1aa174e2211685af68d334ff00ae84739
Maa-ly <lydiaahenkorahgyamfi@gmail.com>
2025-06-25 23:25:58 -0700

    car_registry started

--- contract/src/core/car_registry.sol
+++ contract/src/core/car_registry.sol
@@ -1,8 +1,40 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
-contract CarRegistry {
-    function registercar() external {}
+import "contract/src/oracle/Oracle.sol";
+import {ICarOracle} from "../Interface/oracle/IcarOracle.sol";
+import {IOracleMaster} from "../Interface/oracle/IOracleMaster.sol";
 
-    function registerUndernewOwner() external {}
+contract CarRegistry {
+
+
+    // first timer
+    //register or ownership !! protol can regisrer or general??
+    // car brand --- number avaliable ?? instock 
+    // stake / eth or usdc(bool espression for that ) // collateral on the brand 
+    // merke verifier cloned 
+
+    function registercar(string memory _brand, address oracleAddre, ICarOracle.OracleConfig memory config, address brandAdminAddr) external {
+        IOracleMaster oracleMaster = IOracleMaster(oracleAddre);
+        oracleMaster.registerCarBrand(_brand,config, brandAdminAddr);
+
+        bytes ownershipright = keccak256(msg.send, proofhoash, sign akdd);
+        merk.storfroffg(owneshipt);
+        // provide what??
+// proof of ownership
+    }
+
+/// new ownership rights transfer 
+    function registerUndernewOwner() external {
+        // remove olldownership
+        // give to new owner
+        // add new older to the tree and register the information person 
+    }
+
+    function activate() external {}
+
+    function stake() external {}
+
+
+   // --- register --- state -- activate 
 }
--- contract/src/core/merkle_verifier.sol
+++ contract/src/core/merkle_verifier.sol
@@ -3,4 +3,8 @@ pragma solidity 0.8.28;
 
 contract MerkleVerifier {
 // do this how will it work accross the different chains
+
+// what exatly ??
+
+
 }
