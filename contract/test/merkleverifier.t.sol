5061d574dcd37456745a7147de27e875678e1ed3
kaleel <pixelhubster@gmail.com>
2025-07-02 00:08:06 +0000

    updated merkle

--- contract/test/merkleverifier.t.sol
+++ contract/test/merkleverifier.t.sol
@@ -3,5 +3,10 @@ pragma solidity ^0.8.24;
 
 import {Test} from "forge-std/Test.sol";
 import {MerkleVerifier} from "../src/chainlink/merkle_verifier.sol";
+import {Sync} from "src/chainlink/sync_function.sol";
+import {ProofSync} from "src/chainlink/proofSync.sol";
 
-contract MerkleVerifierTest is Test {}
+// please stop working for a second
+contract MerkleVerifierTest is Test {
+    MerkleVerifier merkle;
+}
